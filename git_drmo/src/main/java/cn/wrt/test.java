2359a4021d0fb73ca79d06e99d9b3e9efed1b381
weiruitao <3134093872@qq>
2020-09-09 16:34:07 +0800

    第一次提交

---
+++ git_drmo/src/main/java/cn/wrt/test.java
@@ -0,0 +1,13 @@
+package cn.wrt;
+
+public class test {
+    public void haha(){
+        System.out.println("hahahahahaahahha");
+
+    }
+
+    //psvm
+    public static void main(String[] args) {
+        System.out.println("as");
+    }
+}
---
+++ git_drmo/src/test/java/cn/wrt/git_drmo/GitDrmoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wrt.git_drmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDrmoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
