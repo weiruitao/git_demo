5acb61d4553b5f15b8c4813b21ccfba37919c60c
weiruitao <3134093872@qq>
2020-09-09 16:45:21 +0800

    第一次修改提交

--- git_demo/src/test/java/cn/wrt/git_demo/test.java
+++ git_demo/src/test/java/cn/wrt/git_demo/test.java
@@ -5,4 +5,8 @@ public class test {
     public void  haha(){
         System.out.println("hahaha");
     }
+
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
 }
